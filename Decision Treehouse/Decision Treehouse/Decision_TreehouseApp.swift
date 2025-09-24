1b2576c82b37d90e8b12dac481af9743ca17b295
Myron Licht <marlamin.pegason.wiz@gmail.com>
2025-09-24 18:40:40 -0400

    Normalize repo: add .gitignore and clean Xcode/macOS noise

---
+++ Decision Treehouse/Decision Treehouse/Decision_TreehouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Decision_TreehouseApp.swift
+//  Decision Treehouse
+//
+//  Created by Myron Licht on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Decision_TreehouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
